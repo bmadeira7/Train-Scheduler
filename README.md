00633658cc8ab6d17fbd1aa9f1cdb39c145d3103
thisisbrix901 <bmadeira7@gmail.com>
2018-09-26 20:07:58 -0400

    done, i think

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Train-Scheduler
 Train Scheduler Homework #7
+
+
+This app will allow the train administrator to input different fields of information regarding the trains they are running. The info goes into the form inputs, 
+then goes into a realtime cloud-based database (firebase), and then those values are calculated and put back into the table element. But when the train admin 
+clicks submit, they get to listen to a classic train related jingle to brighten their miserable day.
---
+++ assets/javascript/app.js
@@ -0,0 +1,86 @@
+var config = {
+    apiKey: "AIzaSyDy8z0fUItzhjapFY6UYBR8DGDbP5O12QE",
+    authDomain: "test-database-4c8a3.firebaseapp.com",
+    databaseURL: "https://test-database-4c8a3.firebaseio.com",
+    projectId: "test-database-4c8a3",
+    storageBucket: "test-database-4c8a3.appspot.com",
+    messagingSenderId: "909685564758"
+}
+
+firebase.initializeApp(config);
+
+
+// Created a variable to reference the database.
+var database = firebase.database();
+
+$("#submitButton").on("click", function () {
+
+    var trainName = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var trainTime = $("#trainTime").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency
+    })
+    audio.volume = 0.03;
+})
+var audio = document.getElementById("trainMusic");
+audio.volume = 0.0;
+
+database.ref().on("child_added", function (snapshot) {
+    // Store this train in an easy to use, well-named variable
+    var train = snapshot.val()
+
+
+
+    // Change the HTML to reflect snapshot
+    var newRow = $("<tr>")
+    var nameCol = $("<td>").text(train.trainName)
+    var destCol = $("<td>").text(train.destination)
+    var freqCol = $("<td>").text(train.frequency)
+
+    newRow.append(nameCol, destCol, freqCol);
+
+    $(".tBody").append(newRow);
+
+
+
+    var firstTimeConverted = moment(train.trainTime, "HH:mm").subtract(1, "years");
+
+
+    var currentTime = moment();
+
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+
+    var timeRemainder = diffTime % train.frequency;
+
+
+    var timeTilNextTrain = train.frequency - timeRemainder;
+
+
+    var nextTrain = moment().add(timeTilNextTrain, "minutes");
+
+
+    var nextTrain2Table = $("<td>").text(nextTrain);
+    var timeTilNextTrain2Table = $("<td>").text(timeTilNextTrain);
+
+    newRow.append(nextTrain2Table, timeTilNextTrain2Table);
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+
+
+
+
+
+
